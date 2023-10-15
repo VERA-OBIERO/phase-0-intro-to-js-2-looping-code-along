9eea58188e3b1a8df83ae94722cd067647a5cbff
VERA-OBIERO <vera.obiero@gmail.com>
2023-10-15 19:01:22 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solutions in this file
+
+const gifts= ["teddy bear", "drone", "doll"];
+
+const nameList = []
+
+
+function namesArray(name){
+    debugger;
+    return nameList.push(name);
+
+}
+
+
+function writeCards(nameList,event){
+
+    for ( let i = 0; i < nameList.length; i++){
+        nameList[i]  = `Thank you, ${nameList[i]}, for the wonderful ${event} gift!`;
+    }
+    return nameList;
+
+}
+
+writeCards();
+
+console.log(nameList)
+
+
+ function countDown(number){
+    let count = 0;
+    while (count <= number){
+        console.log(count++);
+    }
+    return count;
+ }
+
+ countDown(10);
